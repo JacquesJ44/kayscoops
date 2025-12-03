4b05618d700a9c5b60d406415fadd3e187aa850c
Jacques du Toit <jacquesj44@gmail.com>
2025-12-03 18:47:01 +0200

    fix db_ops to reflect correct db file 031225

--- db_ops.py
+++ db_ops.py
@@ -1,7 +1,7 @@
 import sqlite3
 from pathlib import Path
 
-DB_PATH = Path(__file__).parent / "scoop_tracker.db"
+DB_PATH = Path(__file__).parent / "kayscoops.db"
 
 class DBOps:
     def __init__(self, db_path=DB_PATH):
